af393bc62dfa7a0c8f1ca9002215e4ba847cf8e6
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2024-03-18 02:44:47 +0530

    Test GL : Dec Attempt #1/ 0 (Build was able to boot on initial commit `026adfe`).
    • Add Keystore2 related blobs and services.
    • Changed platform version to `99.87.36`.
    • Fix Illformed manifest file.
    • Add more needed libraries.
    • Remove the redundants that causes the spamming of errors on logcat.

--- device.mk
+++ device.mk
@@ -7,23 +7,16 @@
 
 LOCAL_PATH := device/xiaomi/duchamp
 
-# Hidl Service
-PRODUCT_ENFORCE_VINTF_MANIFEST := true
-
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH) 
-
 # Dynamic
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # API
-PRODUCT_SHIPPING_API_LEVEL := 31
+PRODUCT_SHIPPING_API_LEVEL := 32
 PRODUCT_TARGET_VNDK_VERSION := 34
 
 # A/B
 AB_OTA_UPDATER := true
 ENABLE_VIRTUAL_AB := true
-TARGET_ENFORCE_AB_OTA_PARTITION_LIST := true
 AB_OTA_PARTITIONS += \
     apusys \
     audio_dsp \
@@ -62,22 +55,16 @@ AB_OTA_PARTITIONS += \
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
-    FILESYSTEM_TYPE_system=erofs \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-mtkimpl.recovery
-
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
-    android.hardware.boot@1.2-service    
-
-PRODUCT_PACKAGES += \
-    bootctrl.mt6897
+    android.hardware.boot@1.2-mtkimpl.recovery \
+    bootctrl.mt6897 \
+    bootctrl.mt6897.recovery
 
 PRODUCT_PACKAGES := \
     bootctrl.mt6897 \
@@ -126,18 +113,10 @@ PRODUCT_PACKAGES += \
 # Additional Target Libraries
 TARGET_RECOVERY_DEVICE_MODULES += \
     android.hardware.keymaster@4.1 \
-    android.hardware.vibrator-V1-ndk_platform \
-    android.hardware.graphics.common@1.0 \
     libion \
-    libxml2 \
-    android.hardware.health@2.0-impl-default \
-    android.hardware.boot@1.0
+    libxml2
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.vibrator-V1-ndk_platform.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.graphics.common@1.0.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.health@2.0-impl-default.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.boot@1.0.so
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
